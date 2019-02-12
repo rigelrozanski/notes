7c41d8b72c9dd8cc40b6da377bfc5ce1f37f6ad4
rigelrozanski <rigel.rozanski@gmail.com>
2019-02-11 19:04:12 -0500

    add alias functionality

--- main.go
+++ main.go
@@ -12,6 +12,12 @@ import (
 // name of the wb used to hold command notes
 const wbName = "notes"
 
+// structure of notes to be stored
+type Notes struct {
+	Aliases map[string]string   `json:"aliases"`
+	Goodies map[string][]string `json:"goodies"`
+}
+
 func main() {
 	args := os.Args[1:]
 	if len(args) < 1 {
@@ -28,21 +34,31 @@ func main() {
 	}
 
 	// parse the notes file
-	var notes map[string][]string
+	var notes Notes
 	json.Unmarshal(notesBz, &notes)
-	if len(notes) == 0 {
-		fmt.Println("notes wb is not json formatted")
+	if len(notes.Goodies) == 0 {
+		fmt.Printf("debug notes: %v\n", notes)
+		fmt.Println("notes wb is not properly formatted")
 		return
 	}
 
 	// print the notes
-	note := notes[search]
+	note := notes.Goodies[search]
+
+	// attempt to find an alias
+	if len(note) == 0 {
+		revisedSearch := notes.Aliases[search]
+		if len(revisedSearch) > 0 {
+			note = notes.Goodies[revisedSearch]
+		}
+	}
 	if len(note) == 0 {
 		fmt.Println("can't find note")
 		return
 	}
+
 	for _, line := range note {
-		fmt.Println(line)
+		fmt.Printf("    %v\n", line)
 	}
 	fmt.Println("")
 }
